693df5f59c8b915ea4e47570982b646311d51348
Jeison Tomio <medriase@gmail.com>
2023-10-17 02:14:56 -0300

    feat: add header menu using sheet component

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,22 +1,55 @@
 import React from 'react'
 import { Card } from './card'
 import { Button } from './button'
-import { MenuIcon, ShoppingCartIcon } from 'lucide-react'
+import {
+  HomeIcon,
+  ListOrderedIcon,
+  LogInIcon,
+  MenuIcon,
+  PercentIcon,
+  ShoppingCartIcon,
+} from 'lucide-react'
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from './sheet'
 
 export default function Header() {
   return (
-    <div className="container">
-      <Card className="flex justify-between p-[1.875rem] items-center">
-        <Button variant={'outline'} size="icon">
-          <MenuIcon />
-        </Button>
-        <h1 className="font-semibold text-lg">
-          <span className="text-primary">FSW</span> Store
-        </h1>
-        <Button variant={'outline'} size="icon">
-          <ShoppingCartIcon />
-        </Button>
-      </Card>
-    </div>
+    <Card className="flex items-center justify-between p-[1.875rem]">
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button variant={'outline'} size="icon">
+            <MenuIcon />
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left">
+          <SheetHeader className="text-left text-lg font-semibold">
+            Menu
+          </SheetHeader>
+          <div className="mt-2 flex flex-col gap-3">
+            <Button variant={'outline'} className="w-full justify-start gap-2">
+              <HomeIcon />
+              Início
+            </Button>
+            <Button variant={'outline'} className="w-full justify-start gap-2">
+              <PercentIcon />
+              Ofertas
+            </Button>
+            <Button variant={'outline'} className="w-full justify-start gap-2">
+              <ListOrderedIcon />
+              Catalago
+            </Button>
+            <Button variant={'outline'} className="w-full justify-start gap-2">
+              <LogInIcon />
+              Login
+            </Button>
+          </div>
+        </SheetContent>
+      </Sheet>
+      <h1 className="text-lg font-semibold">
+        <span className="text-primary">FSW</span> Store
+      </h1>
+      <Button variant={'outline'} size="icon">
+        <ShoppingCartIcon />
+      </Button>
+    </Card>
   )
 }
